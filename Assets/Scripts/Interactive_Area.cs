baefc4a22c15533159303658287fd8c7ddbafdc3
Matias-Arisi-Guliasdad <49960010@est.ort.edu.ar>
2026-04-30 10:39:48 -0300

    Initial commit

---
+++ Assets/Scripts/Interactive_Area.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive_Area : MonoBehaviour
+{
+   void OnCollisionEnter(Collision papu)
+   {
+    if(papu.gameObject.CompareTag("Pickable"))
+    {
+        Destroy(papu.gameObject);
+    }
+   }
+}
---
+++ Assets/Scripts/ScoreUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
